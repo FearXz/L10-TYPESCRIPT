3c2bb6614da5c40f73a8a342d8fddc9862eba0c5
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-01-18 15:30:42 +0100

    aggiunto commento

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -14,3 +14,20 @@ const store = configureStore({
 });
 
 export default store;
+
+/* import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import fetchArticles from "../reducers/fetchArticles";
+
+export interface RootState {
+  fetchReducer: ReturnType<typeof fetchArticles>;
+}
+
+const rootReducer = combineReducers({
+  fetchReducer: fetchArticles,
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+export default store; */
